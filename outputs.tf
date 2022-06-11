33ba59995bd485a2f07f47a79322f4e8f33780d5
Jakub Scholz <www@scholzj.com>
2022-06-11 21:14:45 +0200

    Remove dependency on the template module to make it usable on Darwin/arm64
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
 output "token" {
   description = "Kubeadm token"
-  value       = data.template_file.kubeadm_token.rendered
+  value       = templatefile("${path.module}/templates/token.tpl", { token1 = join("", random_shuffle.token1.result), token2 = join("", random_shuffle.token2.result) } )
+
+  depends_on = [
+    random_shuffle.token1,
+    random_shuffle.token1,
+  ]
 }
 
